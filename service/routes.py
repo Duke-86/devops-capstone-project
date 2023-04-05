3211fe138cb723d95eb42d29b99171760d103c01
Duke-86 <wac67@comcast.net>
2023-04-05 17:33:16 -0400

    Fixing routes.py again

--- service/routes.py
+++ service/routes.py
@@ -61,9 +61,8 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
-
-
 def list_accounts():
     """
     List all Accounts
@@ -81,9 +80,8 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-
-
 def get_accounts(account_id):
     """
     Reads an Account
@@ -99,9 +97,8 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
-
-
 def update_accounts(account_id):
     """
     Update an Account
@@ -119,9 +116,8 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-
-
 def delete_accounts(account_id):
     """
     Delete an Account
